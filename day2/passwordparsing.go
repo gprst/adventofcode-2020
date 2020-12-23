20b2cf69a2392798908333bc20e885922e0d9758
Gabriel Proust <gabriel.proust@lilo.org>
2020-12-23 19:47:23 +0100

    2nd: rename password parsing helper functions

--- day2/passwordparsing.go
+++ day2/passwordparsing.go
@@ -5,20 +5,20 @@ import (
 	"strings"
 )
 
-func getMinAndMax(minAndMax string) (uint8, uint8) {
-	splitMinAndMax := strings.Split(minAndMax, "-")
-	min, _ := strconv.Atoi(splitMinAndMax[0])
-	max, _ := strconv.Atoi(splitMinAndMax[1])
-	return uint8(min), uint8(max)
+func getKeyNumbers(rawKeyNumbers string) (uint8, uint8) {
+	splitKeyNumbers := strings.Split(rawKeyNumbers, "-")
+	firstKeyNumber, _ := strconv.Atoi(splitKeyNumbers[0])
+	secondKeyNumber, _ := strconv.Atoi(splitKeyNumbers[1])
+	return uint8(firstKeyNumber), uint8(secondKeyNumber)
 }
 
 func getMandatoryLetter(input string) rune {
 	return rune(input[0])
 }
 
-func parseInput(rawSplitInput []string) (min uint8, max uint8, mandatoryLetter rune, password string) {
-	min, max = getMinAndMax(rawSplitInput[0])
+func parseInput(rawSplitInput []string) (firstKeyNumber uint8, secondKeyNumber uint8, mandatoryLetter rune, password string) {
+	firstKeyNumber, secondKeyNumber = getKeyNumbers(rawSplitInput[0])
 	mandatoryLetter = rune(rawSplitInput[1][0])
 	password = rawSplitInput[2]
-	return min, max, mandatoryLetter, password
+	return firstKeyNumber, secondKeyNumber, mandatoryLetter, password
 }
